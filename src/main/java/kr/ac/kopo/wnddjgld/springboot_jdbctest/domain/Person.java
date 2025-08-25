52be9f0df0de9782828914487195b87b949b26f0
wnddjgld <wnddjgld@gmail.com>
2025-08-25 11:49:29 +0900

    2025-08-25

--- src/main/java/kr/ac/kopo/wnddjgld/springboot_jdbctest/domain/Person.java
+++ src/main/java/kr/ac/kopo/wnddjgld/springboot_jdbctest/domain/Person.java
@@ -1,4 +1,11 @@
 package kr.ac.kopo.wnddjgld.springboot_jdbctest.domain;
 
+import lombok.Data;
+
+@Data
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String email;
 }
