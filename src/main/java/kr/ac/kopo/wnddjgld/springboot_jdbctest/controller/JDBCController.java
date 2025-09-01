f853fb43afc01b60d5864b5d71d4e819be2a9db2
wnddjgld <wnddjgld@gmail.com>
2025-09-01 10:27:44 +0900

    2025-08-25

--- src/main/java/kr/ac/kopo/wnddjgld/springboot_jdbctest/controller/JDBCController.java
+++ src/main/java/kr/ac/kopo/wnddjgld/springboot_jdbctest/controller/JDBCController.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,5 +25,22 @@ public class JDBCController {
         List<Person> personList = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Person.class));
         model.addAttribute("personList", personList);
         return "viewPage01";
+    }
+
+    @GetMapping("/new")
+    public String newPage(Model model) {
+        Person person = new Person();
+        model.addAttribute("person", person);
+        return "viewPage01_new";
+    }
+
+    @PostMapping("/insert")
+    public String insertPerson(@ModelAttribute Person person) {
+        String sql = "insert into person (name, age, email) values (?, ?, ?)";
+//        jdbcTemplate.update(sql, person.getName(), person.getAge(), person.getEmail());
+
+        Object[] params = {person.getName(), person.getAge(), person.getEmail()};
+        int resultCount = jdbcTemplate.update(sql, params);
+        return "redirect:/exam01";
     }
 }
