7b02163e45247fd9f50260907c79e16e48aaf5bf
wnddjgld <wnddjgld@gmail.com>
2025-09-15 09:47:32 +0900

    @Query 사용: insert

--- src/main/java/kr/ac/kopo/wnddjgld/springboot_jdbctest/controller/Example03Controller.java
+++ src/main/java/kr/ac/kopo/wnddjgld/springboot_jdbctest/controller/Example03Controller.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +21,17 @@ public class Example03Controller {
         Iterable<Member> memberList = repository.selectMethod();
         model.addAttribute("memberList", memberList);
         return "viewPage03";
+    }
+
+    @GetMapping("/new")
+    public String newMethod(Model model) {
+        model.addAttribute("member", new Member());
+        return "viewPage03_new";
+    }
+
+    @PostMapping("/insert")
+    public String insertMethod(@ModelAttribute("member") Member member) {
+        repository.insertMethod(member.getName(), member.getAge(), member.getEmail());
+        return "redirect:/exam03";
     }
 }
